bc6bfcc64b94888e3df728e0078a8abb2c78f36a
Ryan Williams <ryan.blake.williams@gmail.com>
2015-06-01 16:51:58 +0000

    try to fix BAM/SAM header on cluster

--- adam-core/src/main/scala/org/bdgenomics/adam/rdd/read/AlignmentRecordRDDFunctions.scala
+++ adam-core/src/main/scala/org/bdgenomics/adam/rdd/read/AlignmentRecordRDDFunctions.scala
@@ -130,6 +130,32 @@ class AlignmentRecordRDDFunctions(rdd: RDD[AlignmentRecord])
     // add keys to our records
     val withKey = convertRecords.keyBy(v => new LongWritable(v.get.getAlignmentStart))
 
+    val bcastHeader = rdd.context.broadcast(header)
+    val mp = rdd.mapPartitionsWithIndex((idx, iter) => {
+      log.warn("Setting SAM/BAM header for partition " + idx)
+      val header = bcastHeader.value
+      synchronized {
+        // perform map partition call to ensure that the VCF header is set on all
+        // nodes in the cluster; see:
+        // https://github.com/bigdatagenomics/adam/issues/353
+
+        asSam match {
+          case true  =>
+            ADAMSAMOutputFormat.addHeader(header)
+            log.warn("Set BAM header for partition " + idx)
+          case false =>
+            ADAMBAMOutputFormat.addHeader(header)
+            log.warn("Set BAM header for partition " + idx)
+        }
+      }
+      Iterator[Int]()
+    }).count()
+
+    // force value check, ensure that computation happens
+    if (mp != 0) {
+      log.warn("Had more than 0 elements after map partitions call to set VCF header across cluster.")
+    }
+
     // attach header to output format
     asSam match {
       case true  => ADAMSAMOutputFormat.addHeader(header)
